98cd26ce2c352c000f2ffa520269fe390386adc9
Jonathan Tang <jct19@sfu.ca>
2021-01-22 18:57:38 -0800

    render client list from a template

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,15 +1,15 @@
 # Create your web views here
 import requests
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 from cbrsite.settings import SITE_URL
-from clients.serializer import ClientSerializer
 
 
 class ClientView(View):
     def get(self, request, *args, **kwargs):
         response = requests.get(SITE_URL + "api/clients")
-
-        client = ClientSerializer(data=response.json(), many=True)
-        return HttpResponse("hello world")
+        clients = response.json()
+        return render(request, 'index.html', {
+            "object_list": clients
+        })

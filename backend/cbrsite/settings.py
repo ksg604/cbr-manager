521d8b6edd3d92d4e6de3719d4299f3f94a9fcbe
Jonathan Tang <jct19@sfu.ca>
2021-01-30 14:56:32 -0800

    create authentication app

--- backend/cbrsite/settings.py
+++ backend/cbrsite/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'clients.apps.ClientsConfig',
+    'authenticate.apps.AuthenticateConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

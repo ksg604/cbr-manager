30646164119e12d196b94a5a6a3516f0e7916822
Peter Tran <ttt13@sfu.ca>
2021-01-31 14:48:05 -0800

    add cards for top priority clients, viewpager and adapter, add all clients button

--- frontend/app/src/main/java/com/example/cbr_manager/MainActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cbr_manager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(startIntent);
             }
         });
     }

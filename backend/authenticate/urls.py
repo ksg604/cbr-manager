0af02c64a6d80d9e9863a7bee0e405822853eb09
Jonathan Tang <jct19@sfu.ca>
2021-02-02 12:42:50 -0800

    use single quotes for consistency

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -3,5 +3,5 @@ from rest_framework.authtoken import views as auth_view
 
 app_name = 'authenticate'
 urlpatterns = [
-    path('token-auth/', auth_view.obtain_auth_token, name="token-auth")
+    path('token-auth/', auth_view.obtain_auth_token, name='token-auth')
 ]

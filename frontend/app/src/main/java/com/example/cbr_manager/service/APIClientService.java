19bb6dc1976dca7e44a267153d1191e48ce94b6d
Jonathan Tang <jct19@sfu.ca>
2021-02-02 16:50:35 -0800

    create a client service object
    the service should have access to all other services as an "api client"

--- frontend/app/src/main/java/com/example/cbr_manager/service/APIClientService.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/APIClientService.java
@@ -1,11 +1,26 @@
 package com.example.cbr_manager.service;
 
+import com.example.cbr_manager.service.auth.AuthService;
+import com.example.cbr_manager.service.auth.AuthToken;
+import com.example.cbr_manager.service.auth.LoginUserPass;
+
+
 public class APIClientService {
+    private final AuthService authService;
 
-    private AuthService service;
-
-    public APIClientService(AuthService service) {
-        this.service = service;
+    public APIClientService(LoginUserPass loginUserPass) {
+        // initialize all API services
+        authService = new AuthService(loginUserPass);
     }
 
+    public void authenticate() {
+        authService.registerToken();
+    }
+
+    public AuthToken getAuthToken(){
+        return authService.getAuthToken();
+    }
+
+    public void otherAPIMethods(){
+    }
 }

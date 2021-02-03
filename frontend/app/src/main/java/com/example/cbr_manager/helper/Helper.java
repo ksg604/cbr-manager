f4757f641f6293ffb1f9af2ad3d0f27df1f5e5c0
Jonathan Tang <jct19@sfu.ca>
2021-02-02 16:45:22 -0800

    create helper to fetch api url from local.properties

---
+++ frontend/app/src/main/java/com/example/cbr_manager/helper/Helper.java
@@ -0,0 +1,10 @@
+package com.example.cbr_manager.helper;
+
+import com.example.cbr_manager.BuildConfig;
+
+
+public class Helper {
+    public static String getBaseUrl() {
+        return BuildConfig.API_URL;
+    }
+}

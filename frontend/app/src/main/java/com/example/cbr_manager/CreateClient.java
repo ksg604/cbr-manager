0e6e9888d8b8ceed3a3bd6d47a1ba1551ad20266
kpa43 <arthur_park@sfu.ca>
2021-02-03 15:26:13 -0800

    added consent, id #, location and village number to new client page

--- frontend/app/src/main/java/com/example/cbr_manager/CreateClient.java
+++ frontend/app/src/main/java/com/example/cbr_manager/CreateClient.java
@@ -1,4 +1,52 @@
 package com.example.cbr_manager;
 
-public class CreateClient {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateClient extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Spinner spinner;
+    private static final String[] paths = {"BidiBidi Zone 1", "BidiBidi Zone 2", "BidiBidi Zone 3", "BidiBidi Zone 4", "BidiBidi Zone 5",
+                                            "Palorinya Basecamp", "Palorinya Zone 1", "Palorinya Zone 2", "Palorinya Zone 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_client);
+
+        spinner = (Spinner)findViewById(R.id.location_dropdown);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(CreateClient.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
 }

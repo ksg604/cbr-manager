f2e870b8fa4908ae226203d593ee82cf4622456b
Jonathan Tang <jct19@sfu.ca>
2021-02-04 15:58:41 -0800

    change to get request

--- frontend/app/src/main/java/com/example/cbr_manager/service/client/ClientAPI.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/client/ClientAPI.java
@@ -1,18 +1,15 @@
 package com.example.cbr_manager.service.client;
 
-import com.example.cbr_manager.service.auth.AuthToken;
-import com.example.cbr_manager.service.auth.LoginUserPass;
-
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.POST;
 
 /*
 Define our API request endpoints here
  */
 public interface ClientAPI {
-    @POST("api/token-auth/")
+    @GET("api/clients/")
     Call<List<Client>> getClients(@Header("Authorization") String authHeader);
 }

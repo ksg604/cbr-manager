47305ed03fb4a1cd969989701e758898f18c3f31
Jonathan Tang <jct19@sfu.ca>
2021-02-04 15:37:05 -0800

    change api service into a singleton

--- frontend/app/src/main/java/com/example/cbr_manager/service/APIService.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/APIService.java
@@ -5,23 +5,48 @@ import com.example.cbr_manager.service.auth.AuthToken;
 import com.example.cbr_manager.service.auth.LoginUserPass;
 import com.example.cbr_manager.service.client.ClientService;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class APIService {
-    public AuthService authService;
 
-    public ClientService clientService;
+    public static AuthService authService;
+    public static ClientService clientService;
+    private static APIService INSTANCE = null;
 
-    public APIService(LoginUserPass loginUserPass) {
-        this.authenticate(loginUserPass);
-
-        // initialize all API services
-        AuthToken token = this.authService.getAuthToken();
-
-        clientService = new ClientService(token);
+    private APIService() {
     }
 
-    private void authenticate(LoginUserPass loginUserPass){
+    public static APIService getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new APIService();
+        }
+        return (INSTANCE);
+    }
+
+    private static void authenticate(LoginUserPass loginUserPass) {
         authService = new AuthService(loginUserPass);
-        this.authService.fetchAuthToken();
+        authService.fetchAuthToken().enqueue(new Callback<AuthToken>() {
+            @Override
+            public void onResponse(Call<AuthToken> call, Response<AuthToken> response) {
+                if (response.isSuccessful()){
+                    AuthToken token = response.body();
+
+                    authService.setAuthToken(token);
+                    initializeServices(token);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AuthToken> call, Throwable t) {
+                // Todo: not sure proper method of error handling
+            }
+        });
+    }
+
+    private static void initializeServices(AuthToken token){
+        clientService = new ClientService(token);
     }
 }

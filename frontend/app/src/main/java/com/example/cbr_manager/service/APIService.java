175e06f2f19d5e33cc15ad1d9192f0f622b90dcd
Jonathan Tang <jct19@sfu.ca>
2021-02-04 15:19:21 -0800

    add the client server to api service

--- frontend/app/src/main/java/com/example/cbr_manager/service/APIService.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/APIService.java
@@ -1,18 +1,27 @@
 package com.example.cbr_manager.service;
 
 import com.example.cbr_manager.service.auth.AuthService;
+import com.example.cbr_manager.service.auth.AuthToken;
 import com.example.cbr_manager.service.auth.LoginUserPass;
+import com.example.cbr_manager.service.client.ClientService;
 
 
 public class APIService {
-    public final AuthService authService;
+    public AuthService authService;
+
+    public ClientService clientService;
 
     public APIService(LoginUserPass loginUserPass) {
+        this.authenticate(loginUserPass);
+
         // initialize all API services
-        authService = new AuthService(loginUserPass);
+        AuthToken token = this.authService.getAuthToken();
+
+        clientService = new ClientService(token);
     }
 
-    public void authenticate() {
-        authService.fetchAuthToken();
+    private void authenticate(LoginUserPass loginUserPass){
+        authService = new AuthService(loginUserPass);
+        this.authService.fetchAuthToken();
     }
 }

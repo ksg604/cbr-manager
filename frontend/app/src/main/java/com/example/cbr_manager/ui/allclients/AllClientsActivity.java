7253d1d6d1f9911c4188afe2de3ab5d701111d05
Jonathan Tang <jct19@sfu.ca>
2021-02-04 16:15:19 -0800

    dynamic pull and list clients

--- frontend/app/src/main/java/com/example/cbr_manager/ui/allclients/AllClientsActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/allclients/AllClientsActivity.java
@@ -1,57 +1,73 @@
 package com.example.cbr_manager.ui.allclients;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.example.cbr_manager.R;
+import com.example.cbr_manager.service.APIService;
+import com.example.cbr_manager.service.client.Client;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AllClientsActivity extends AppCompatActivity implements ClientRecyclerItemAdapter.OnItemListener {
+
+
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    ArrayList<ClientRecyclerItem> clientRecyclerItems;
+    private RecyclerView.Adapter adapter;
+    private ArrayList<ClientRecyclerItem> clientRecyclerItems = new ArrayList<>();
+
+    private APIService apiService = APIService.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_clients);
 
-        // Test data for now before backend implementation.
-        clientRecyclerItems = new ArrayList<>();
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "Burnaby Region"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Leonhard Euler", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Leonhard Euler", "Vancouver Region"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "Greater Vancouver"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Leonhard Euler", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Leonhard Euler", "Vancouver Region"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Leonhard Euler", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Leonhard Euler", "Vancouver Region"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "Mission B.C"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "Surrey Region"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "Fraser Valley"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "SFU"));
-        clientRecyclerItems.add(new ClientRecyclerItem(R.drawable.dog, "Peter Tran", "SFU"));
-
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true); // if we know it won't change size.
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ClientRecyclerItemAdapter(clientRecyclerItems, this);
+        adapter = new ClientRecyclerItemAdapter(clientRecyclerItems, this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(adapter);
+
+        fetchClientsToList(clientRecyclerItems);
     }
 
     @Override
     public void onItemClick(int position) {
         ClientRecyclerItem clientRecyclerItem = clientRecyclerItems.get(position);
         Toast.makeText(this, "Item " + position + " selected.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void fetchClientsToList(List<ClientRecyclerItem> clientUIList) {
+        if (apiService.isAuthenticated()) {
+            apiService.clientService.getClients().enqueue(new Callback<List<Client>>() {
+                @Override
+                public void onResponse(Call<List<Client>> call, Response<List<Client>> response) {
+                    if (response.isSuccessful()) {
+                        List<Client> clientList = response.body();
+                        for (Client client : clientList) {
+                            clientUIList.add(new ClientRecyclerItem(R.drawable.dog, client.getFullName(), "Burnaby Region"));
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onFailure(Call<List<Client>> call, Throwable t) {
+
+                }
+            });
+        }
     }
 }

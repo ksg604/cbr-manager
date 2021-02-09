581c438d497bef87c33d8ce5434622ca53b49f42
Jonathan Tang <jct19@sfu.ca>
2021-02-08 18:53:53 -0800

    create visit model

--- backend/visits/models.py
+++ backend/visits/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from clients.models import Client
+
+
+class Visit(models.Model):
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    user_creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    client_state_previous = models.JSONField()
+    client_state_updated = models.JSONField()
+
+    additional_notes = models.TextField(verbose_name="additional notes")

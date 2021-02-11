16e566be1cab189b58184f73eed591d3b512dee1
Peter Tran <ttt13@sfu.ca>
2021-02-10 20:53:49 -0800

    code cleanup in fragments my deleting dead code

--- frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/PreambleFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/PreambleFragment.java
@@ -13,42 +13,15 @@ import android.view.ViewGroup;
 
 import com.example.cbr_manager.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PreambleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PreambleFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public PreambleFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PreambleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PreambleFragment newInstance(String param1, String param2) {
-        PreambleFragment fragment = new PreambleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override

d00ae2459a660bd2e1bcc8eeb47fd1f8003a8870
Jonathan Tang <jct19@sfu.ca>
2021-02-10 22:22:55 -0800

    update VisitViewSet
    - Restrict updates on this model
    - Allow filtering by client_id when listing

--- backend/visits/views.py
+++ backend/visits/views.py
@@ -1,10 +1,20 @@
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from visits.models import Visit
 from visits.serializer import VisitSerializer
 
 
-class VisitViewSet(viewsets.ModelViewSet):
+class VisitViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.RetrieveModelMixin,
+                   viewsets.GenericViewSet):
     serializer_class = VisitSerializer
     queryset = Visit.objects.all()
+
+    def get_queryset(self):
+        queryset = Visit.objects.all()
+        client_id = self.request.query_params.get('client_id', None)
+        if client_id is not None:
+            return queryset.filter(client__id=client_id)
+        return queryset

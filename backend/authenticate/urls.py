d8aeb439e610153eb57ccbfd5376057323d8a739
Jonathan Tang <jct19@sfu.ca>
2021-02-11 16:53:09 -0800

    specify api/token-auth explicitly in the root url path

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from rest_framework.authtoken import views as auth_view
 
 app_name = 'authenticate'
-urlpatterns = [
-    path('token-auth/', auth_view.obtain_auth_token, name='token-auth')
-]
+urlpatterns = []

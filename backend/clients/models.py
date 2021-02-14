8577956676ececae3dc584f622e66998fc7c8170
Peter Tran <ttt13@sfu.ca>
2021-02-13 23:40:40 -0800

    add visit fields to client model

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -35,9 +35,66 @@ class Client(models.Model):
     social_require = models.TextField(blank=True)
     social_goal = models.TextField(blank=True)
 
+    # Visits
+    is_cbr_purpose = models.BooleanField(blank=True, default=False)
+    is_disability_referral_purpose = models.BooleanField(blank=True, default=False)
+    is_disability_follow_up_purpose = models.BooleanField(blank=True, default=False)
+
+    is_health_provision = models.BooleanField(blank=True, default=False)
+    is_education_provision = models.BooleanField(blank=True, default=False)
+    is_social_provision = models.BooleanField(blank=True, default=False)
+
+    visit_date = models.DateField(auto_now_add=True)
+    cbr_worker_name = models.TextField(blank=True, max_length=30)
+
+    location_visit_gps = models.TextField(blank=True, max_length=30)
+    location_drop_down = models.TextField(blank=True, max_length=30)
+    village_no_visit = models.IntegerField(blank=True, default=0)
+
+    wheelchair_health_provision = models.BooleanField(blank=True, default=False)
+    prosthetic_health_provision = models.BooleanField(blank=True, default=False)
+    orthotic_health_provision = models.BooleanField(blank=True, default=False)
+    repairs_health_provision = models.BooleanField(blank=True, default=False)
+    referral_health_provision = models.BooleanField(blank=True, default=False)
+    advice_health_provision = models.BooleanField(blank=True, default=False)
+    advocacy_health_provision = models.BooleanField(blank=True, default=False)
+    encouragement_health_provision = models.BooleanField(blank=True, default=False)
+
+    wheelchair_health_provision_text = models.TextField(blank=True, max_length=100)
+    prosthetic_health_provision_text = models.TextField(blank=True, max_length=100)
+    orthotic_health_provision_text = models.TextField(blank=True, max_length=100)
+    repairs_health_provision_text = models.TextField(blank=True, max_length=100)
+    referral_health_provision_text = models.TextField(blank=True, max_length=100)
+    advice_health_provision_text = models.TextField(blank=True, max_length=100)
+    advocacy_health_provision_text = models.TextField(blank=True, max_length=100)
+    encouragement_health_provision_text = models.TextField(blank=True, max_length=100)
+
+    goal_met_health_provision = models.TextField(blank=True, max_length=30)
+    conclusion_health_provision = models.TextField(blank=True, max_length=100)
+
+    advice_education_provision = models.BooleanField(blank=True, default=False)
+    advocacy_education_provision = models.BooleanField(blank=True, default=False)
+    referral_education_provision = models.BooleanField(blank=True, default=False)
+    encouragement_education_provision = models.BooleanField(blank=True, default=False)
+
+    advice_education_provision_text = models.TextField(blank=True, max_length=100)
+    advocacy_education_provision_text = models.TextField(blank=True, max_length=100)
+    referral_education_provision_text = models.TextField(blank=True, max_length=100)
+    encouragement_education_provision_text = models.TextField(blank=True, max_length=100)
+
+    goal_met_education_provision = models.TextField(blank=True, max_length=30)
+    conclusion_education_provision = models.TextField(blank=True, max_length=100)
+
+    advice_social_provision = models.BooleanField(blank=True, default=False)
+    advocacy_social_provision = models.BooleanField(blank=True, default=False)
+    referral_social_provision = models.BooleanField(blank=True, default=False)
+    encouragement_social_provision = models.BooleanField(blank=True, default=False)
+
+    goal_met_social_provision = models.BooleanField(blank=True, default=False)
+    conclusion_social_provision = models.BooleanField(blank=True, default=False)
+
     class Meta:
         ordering = ['id']
-
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)

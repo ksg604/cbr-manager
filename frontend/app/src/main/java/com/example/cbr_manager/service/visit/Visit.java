18927bb1f9ee881ca30f2422994e7cc23cd6dfe0
Peter Tran <ttt13@sfu.ca>
2021-02-14 21:50:56 -0800

    correcting fields for visit API

--- frontend/app/src/main/java/com/example/cbr_manager/service/visit/Visit.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/visit/Visit.java
@@ -2,14 +2,25 @@ package com.example.cbr_manager.service.visit;
 
 import com.example.cbr_manager.service.client.Client;
 import com.example.cbr_manager.service.user.User;
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Visit {
 
     @SerializedName("additional_info")
+    @Expose
     private String additionalInfo;
 
+    @SerializedName("client_id")
+    @Expose
+    private int clientID;
+
+    @SerializedName("user_id")
+    @Expose
+    private int userID;
+
+    @SerializedName("client")
+    @Expose
     private Client client;
 
-    private User user;
 }

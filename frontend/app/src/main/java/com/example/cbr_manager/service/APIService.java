085003eb95591cc8c60efe70473a02fcc57add8b
ach16 <ach16@sfu.ca>
2021-02-15 17:07:15 -0800

    auto saves user information on login, added getters in apiservice to get user id , email and username

--- frontend/app/src/main/java/com/example/cbr_manager/service/APIService.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/APIService.java
@@ -1,10 +1,18 @@
 package com.example.cbr_manager.service;
 
+import android.util.Log;
+
+import com.example.cbr_manager.R;
 import com.example.cbr_manager.service.auth.AuthService;
 import com.example.cbr_manager.service.auth.AuthToken;
 import com.example.cbr_manager.service.auth.LoginUserPass;
+import com.example.cbr_manager.service.client.Client;
 import com.example.cbr_manager.service.client.ClientService;
+import com.example.cbr_manager.service.user.User;
 import com.example.cbr_manager.service.user.UserService;
+import com.example.cbr_manager.ui.clientlist.ClientListRecyclerItem;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,6 +25,9 @@ public class APIService {
     public AuthService authService;
     public ClientService clientService;
     public UserService userService;
+    public String username;
+    public int userid;
+    public String email;
 
     private APIService() {
     }
@@ -48,6 +59,41 @@ public class APIService {
                 listener.onFailure(call, t);
             }
         });
+    }
+
+    public void storeUserInfo(){
+        userService.getUsers().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if (response.isSuccessful()) {
+                    List<User> userList = response.body();
+                    for (User user : userList) {
+                        if (username.equals(user.getUsername())){
+                            email = user.getEmail();
+                            userid = user.getId();
+                            break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public int getUserid(){
+        return userid;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getEmail(){
+        return email;
     }
 
     public void initializeServices(AuthToken token) {

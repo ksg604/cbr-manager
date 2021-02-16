17fdff75aa8021935a1263bf7d51017203bf8eb9
Jonathan Tang <jct19@sfu.ca>
2021-02-15 17:16:28 -0800

    attach user to the authtoken response

--- backend/authenticate/views.py
+++ backend/authenticate/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
+from users.serializers import UserSerializer
+
+
+class CustomObtainToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainToken, self).post(request, *args, **kwargs)
+
+        # attach user to response
+        token = response.data["token"]
+        user = Token.objects.get(key=token).user
+
+        response.data["user"] = UserSerializer(user).data
+
+        return response

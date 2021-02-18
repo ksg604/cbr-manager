ce6582af2cdfb9571a1a23e9bd567e2e748ad515
ach16 <ach16@sfu.ca>
2021-02-17 17:54:53 -0800

    add Alerts app in backend

--- backend/alerts/views.py
+++ backend/alerts/views.py
@@ -8,7 +8,3 @@ from alerts.serializer import AlertSerializer
 class AlertViewSet(viewsets.ModelViewSet):
     serializer_class = AlertSerializer
     queryset = Alert.objects.all()
-
-    def get_queryset(self):
-        queryset = Alert.objects.all()
-        return queryset

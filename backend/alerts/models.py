245f2a495bacdf8f1a737a8e2752a813f8ee05b4
ach16 <ach16@sfu.ca>
2021-02-19 14:38:49 -0800

    fix text body in alert creation and details to be multiline

--- backend/alerts/models.py
+++ backend/alerts/models.py
@@ -8,3 +8,6 @@ class Alert(models.Model):
     title = models.TextField(max_length=200, blank=True, null=True)
     body = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']

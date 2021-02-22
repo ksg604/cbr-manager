f4ad6f9f4f571ef4556b329e9e5556513059dc82
Jonathan Tang <jct19@sfu.ca>
2021-02-21 18:35:53 -0800

    Fix user login password
    - hash user passwords on user input
    - do not show the user passwords when viewing the user api

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,9 +8,11 @@ from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-    queryset = User.objects.all()
 
     @action(detail=False, methods=["get"], name="Get Current User")
     def current_user(self, request):
         serialized_user = self.serializer_class(request.user)
         return Response(serialized_user.data)
+
+    def get_queryset(self):
+        return User.objects.all()

c8c5df530f9fc1c02a75ef48070e0429dac601dc
Jonathan Tang <jct19@sfu.ca>
2021-02-26 15:40:23 -0800

    define deserializer for referral

---
+++ frontend/app/src/main/java/com/example/cbr_manager/service/referral/ReferralSerializer.java
@@ -0,0 +1,54 @@
+package com.example.cbr_manager.service.referral;
+
+import com.example.cbr_manager.service.referral.ServiceDetails.PhysiotherapyServiceDetail;
+import com.example.cbr_manager.service.referral.ServiceDetails.ServiceDetail;
+import com.example.cbr_manager.service.referral.ServiceDetails.WheelchairServiceDetail;
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferralSerializer implements JsonDeserializer<Referral> {
+
+    // Declaring the static map
+    private static Map<String, Class> map;
+
+    // Instantiating the static map
+    static {
+        map = new HashMap<>();
+        map.put("Wheelchair", WheelchairServiceDetail.class);
+        map.put("Physiotherapy", PhysiotherapyServiceDetail.class);
+    }
+
+    @Override
+    public Referral deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+        Gson gson = new Gson();
+        Referral referral = gson.fromJson(json, Referral.class);
+
+        ServiceDetail serviceDetail = deserializeServiceDetail((JsonObject) json);
+        referral.setServiceDetail(serviceDetail);
+
+        return referral;
+    }
+
+    private ServiceDetail deserializeServiceDetail(JsonObject json) {
+        Gson gson = new Gson();
+
+        JsonObject referral_json = (JsonObject) json.get("referral");
+        String referral_type = json.get("referral_type").getAsString();
+        Class<ServiceDetail> serializer_class =  map.get(referral_type);
+
+        if (serializer_class != null) {
+            return gson.fromJson(referral_json, serializer_class);
+        } else {
+            throw new JsonParseException("Could not serialize service detail.");
+        }
+    }
+}

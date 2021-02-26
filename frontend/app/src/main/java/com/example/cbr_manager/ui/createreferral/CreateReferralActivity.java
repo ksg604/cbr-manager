fce1dfc8eac370a8c419179995974aa4ff78ffb7
Peter Tran <ttt13@sfu.ca>
2021-02-25 18:47:25 -0800

    init package and activity

---
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/createreferral/CreateReferralActivity.java
@@ -0,0 +1,16 @@
+package com.example.cbr_manager.ui.createreferral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cbr_manager.R;
+
+public class CreateReferralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_referral);
+    }
+}

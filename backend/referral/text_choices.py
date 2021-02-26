fe0184188d59f10f5cdd1ef3bcbf0eaa2c7ba38b
Jonathan Tang <jct19@sfu.ca>
2021-02-26 11:53:27 -0800

    define text choices

---
+++ backend/referral/text_choices.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class InjuryLocation(models.TextChoices):
+    BELOW = 'Below'
+    ABOVE = 'Above'
+
+
+class UsageExperience(models.TextChoices):
+    BASIC = 'Basic',
+    INTERMEDIATE = 'Intermediate'
+
+
+class Conditions(models.TextChoices):
+    AMPUTEE = 'Amputee',
+    POLIO = 'Polio',
+    SPINAL_CORD_INJURY = 'Spinal Cord Injury'
+    CEREBRAL_PALSY = 'Cerebral Palsy'
+    SPINA_BIFIDA = 'Spina Bifida'
+    HYDROCEPHALUS = 'Hydrocephalus'
+    VISUAL_IMPAIRMENT = 'Visual Impairment'
+    HEARING_IMPAIRMENT = 'Hearing Impairment'
+    OTHER = 'Other'
+
+
+class ReferralStatus(models.TextChoices):
+    CREATED = 'CREATED'
+    RESOLVED = 'RESOLVED'
+
+
+class ReferralTypes(models.TextChoices):
+    WHEELCHAIR = "Wheelchair"
+    PHYSIOTHERAPY = "Physiotherapy"
+    PROSTHETIC = "Prosthetic"
+    ORTHOTIC = "Orthotic"

7f556121ff2c2ef38e6b6f0a1c0400abda5536c1
Jonathan Tang <jct19@sfu.ca>
2021-02-26 15:39:36 -0800

    overwrite getAPI with a custom serializer

--- frontend/app/src/main/java/com/example/cbr_manager/service/referral/ReferralService.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/referral/ReferralService.java
@@ -2,10 +2,14 @@ package com.example.cbr_manager.service.referral;
 
 import com.example.cbr_manager.service.BaseService;
 import com.example.cbr_manager.service.auth.AuthResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ReferralService extends BaseService {
 
@@ -18,5 +22,21 @@ public class ReferralService extends BaseService {
 
     public Call<List<Referral>> getReferrals() {
         return referralAPI.getReferrals(authHeader);
+    }
+
+    @Override
+    protected ReferralAPI getAPI() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.excludeFieldsWithModifiers();
+
+        ReferralSerializer referralSerializer = new ReferralSerializer();
+        gsonBuilder.registerTypeAdapter(Referral.class, referralSerializer);
+
+        Gson gson = gsonBuilder.create();
+
+        return (ReferralAPI) new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build().create(retroFitAPIClass);
     }
 }

3a68326a01ffa38fd4ac38f6c4478bddd3ad0a21
Jonathan Tang <jct19@sfu.ca>
2021-02-26 11:54:45 -0800

    register referral viewset to urls

--- backend/referral/views.py
+++ backend/referral/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-# Create your views here.
+from referral.models import Referral
+from referral.serializer import ReferralSerializer
+
+
+class ReferralViewSet(ModelViewSet):
+    serializer_class = ReferralSerializer
+
+    def get_queryset(self):
+        queryset = Referral.objects.all()
+        return queryset

8d4c980855c39b2f83a1a75b37b4727e27b93cea
Peter Tran <ttt13@sfu.ca>
2021-02-27 11:29:04 -0800

    setup radio group for services to set visible layouts

--- frontend/app/src/main/java/com/example/cbr_manager/ui/createreferral/CreateReferralActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/createreferral/CreateReferralActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 import com.example.cbr_manager.R;
 
@@ -28,7 +30,53 @@ public class CreateReferralActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        RadioGroup serviceRequired = findViewById(R.id.createReferralServiceRadioGroup);
+        serviceRequired.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                LinearLayout physioLayout = findViewById(R.id.referralPhysioLayout);
+                LinearLayout prostheticLayout = findViewById(R.id.referralProstheticLayout);
+                LinearLayout orthoticLayout = findViewById(R.id.referralOrthoticLayout);
+                LinearLayout wheelchairLayout = findViewById(R.id.referralWheelchairLayout);
+                LinearLayout otherLayout = findViewById(R.id.referralOrthoticLayout);
+
+                if (checkedId == R.id.referralPhysioRadioButton) {
+                    setAllGone();
+                    physioLayout.setVisibility(View.VISIBLE);
+                } else if (checkedId == R.id.referralProstheticRadioButton) {
+                    setAllGone();
+                    prostheticLayout.setVisibility(View.VISIBLE);
+                } else if (checkedId == R.id.referralOrthoticRadioButton) {
+                    setAllGone();
+                    orthoticLayout.setVisibility(View.VISIBLE);
+                } else if (checkedId == R.id.referralWheelChairRadioButton) {
+                    setAllGone();
+                    wheelchairLayout.setVisibility(View.VISIBLE);
+                } else if (checkedId == R.id.referralOtherRadioButton) {
+                    setAllGone();
+                    otherLayout.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
     }
+
+    private void setAllGone() {
+        LinearLayout physioLayout = findViewById(R.id.referralPhysioLayout);
+        LinearLayout prostheticLayout = findViewById(R.id.referralProstheticLayout);
+        LinearLayout orthoticLayout = findViewById(R.id.referralOrthoticLayout);
+        LinearLayout wheelchairLayout = findViewById(R.id.referralWheelchairLayout);
+        LinearLayout otherLayout = findViewById(R.id.referralOrthoticLayout);
+
+        physioLayout.setVisibility(View.GONE);
+        prostheticLayout.setVisibility(View.GONE);
+        orthoticLayout.setVisibility(View.GONE);
+        wheelchairLayout.setVisibility(View.GONE);
+        otherLayout.setVisibility(View.GONE);
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {

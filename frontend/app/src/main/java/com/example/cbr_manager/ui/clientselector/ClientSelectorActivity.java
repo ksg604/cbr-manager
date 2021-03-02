16152b8690666ad2bfe23c308371c78b47f5ca1f
Peter Tran <ttt13@sfu.ca>
2021-03-02 12:57:21 -0800

    init activity, link fragment

---
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/clientselector/ClientSelectorActivity.java
@@ -0,0 +1,16 @@
+package com.example.cbr_manager.ui.clientselector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cbr_manager.R;
+
+public class ClientSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_selector);
+    }
+}

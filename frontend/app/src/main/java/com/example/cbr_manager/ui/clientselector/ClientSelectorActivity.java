b1316939f9efb404e37632502994591c9444d65c
Peter Tran <ttt13@sfu.ca>
2021-03-02 17:54:33 -0800

    enable back button

--- frontend/app/src/main/java/com/example/cbr_manager/ui/clientselector/ClientSelectorActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/clientselector/ClientSelectorActivity.java
@@ -1,8 +1,10 @@
 package com.example.cbr_manager.ui.clientselector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.cbr_manager.R;
 
@@ -12,6 +14,15 @@ public class ClientSelectorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_selector);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Select a Client");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

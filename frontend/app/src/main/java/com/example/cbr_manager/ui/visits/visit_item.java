1b2d98b2a05a1db7339004560d8f866af620a53a
seananigan <sdales@sfu.ca>
2021-03-03 23:12:00 -0800

    change location of visit_item to visits directory

--- frontend/app/src/main/java/com/example/cbr_manager/ui/visits/visit_item.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/visits/visit_item.java
@@ -1,7 +1,8 @@
-package com.example.cbr_manager;
+package com.example.cbr_manager.ui.visits;
 
 import android.os.Bundle;
 
+import com.example.cbr_manager.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 

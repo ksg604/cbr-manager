1611903be4b0447db8a3f10bb025bb60df77bdad
Peter Tran <ttt13@sfu.ca>
2021-03-07 17:57:53 -0800

    remove dead code

--- frontend/app/src/main/java/com/example/cbr_manager/ui/clientdetails/ClientDetailsEditFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/clientdetails/ClientDetailsEditFragment.java
@@ -26,19 +26,10 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClientDetailsEditFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ClientDetailsEditFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -54,15 +45,6 @@ public class ClientDetailsEditFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClientDetailsEditFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ClientDetailsEditFragment newInstance(String param1, String param2) {
         ClientDetailsEditFragment fragment = new ClientDetailsEditFragment();
         Bundle args = new Bundle();

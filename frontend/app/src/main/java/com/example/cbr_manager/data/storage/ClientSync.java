bfe84a380eaaacaf9932cb20606dddc47819196e
ftran <ftran@anywhere.com>
2021-03-08 21:19:18 -0800

    Changed to clientCreatManual, still not working

--- frontend/app/src/main/java/com/example/cbr_manager/data/storage/ClientSync.java
+++ frontend/app/src/main/java/com/example/cbr_manager/data/storage/ClientSync.java
@@ -29,6 +29,7 @@ public class ClientSync {
     private static ClientDBService localdb;
     private static APIService apiService;
     private static ClientSync Instance;
+
     private static volatile List<Client> localClient = new ArrayList<>();
     private static volatile List<Client> serverClient = new ArrayList<>();
 

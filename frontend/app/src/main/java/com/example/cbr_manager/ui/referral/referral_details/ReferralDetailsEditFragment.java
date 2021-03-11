1bbd7fb4b4c2b567add7cbc040abf4dfc53b1220
ach16 <ach16@sfu.ca>
2021-03-10 19:31:54 -0800

    fix ReferralList to update on backpressed

--- frontend/app/src/main/java/com/example/cbr_manager/ui/referral/referral_details/ReferralDetailsEditFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/referral/referral_details/ReferralDetailsEditFragment.java
@@ -12,12 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.cbr_manager.R;
 import com.example.cbr_manager.service.APIService;
 import com.example.cbr_manager.service.client.Client;
+import com.example.cbr_manager.service.referral.Referral;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,122 +27,76 @@ import retrofit2.Response;
 
 public class ReferralDetailsEditFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-
     private APIService apiService = APIService.getInstance();
     private View parentLayout;
-    private Spinner genderSpinner;
-    String gender="";
-    Client client;
-    private static final String[] paths = {"Male", "Female"};
+    private Spinner statusSpinner;
+    String status="";
 
 
     public ReferralDetailsEditFragment() {
         // Required empty public constructor
     }
 
-    public static ReferralDetailsEditFragment newInstance(String param1, String param2) {
-        ReferralDetailsEditFragment fragment = new ReferralDetailsEditFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-        View root = inflater.inflate(R.layout.fragment_client_details_edit, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_referral_details_edit, container, false);
         parentLayout = root.findViewById(android.R.id.content);
 
         Intent intent = getActivity().getIntent();
-        int clientId = intent.getIntExtra("clientId", -1);
+        int referralId = intent.getIntExtra("referralId", -1);
 
-        setupGenderSpinner(root);
-        setupEditTexts(clientId, root);
+        setupStatusSpinner(root);
+        setupTexts(referralId, root);
         setupButtons(root);
-
 
         return root;
     }
 
-    private void modifyClientInfo(Client client) {
+    private void modifyReferralInfo(Referral referral) {
 
-
-        apiService.clientService.modifyClient(client).enqueue(new Callback<Client>() {
+        apiService.referralService.updateReferral(referral).enqueue(new Callback<Referral>() {
             @Override
-            public void onResponse(Call<Client> call, Response<Client> response) {
-                Client client = response.body();
-                Log.d("log",client.getFirstName());
+            public void onResponse(Call<Referral> call, Response<Referral> response) {
                 getActivity().onBackPressed();
             }
             @Override
-            public void onFailure(Call<Client> call, Throwable t) {
+            public void onFailure(Call<Referral> call, Throwable t) {
+            }
+        });
+    }
+
+    private void getAndModifyReferral(int referralId, View root) {
+        EditText editReferTo = (EditText) root.findViewById(R.id.referralDetailsReferToEditTextView);
+        EditText editOutcome = (EditText) root.findViewById(R.id.referralDetailsOutcomeEditTextView);
+
+        apiService.referralService.getReferral(referralId).enqueue(new Callback<Referral>() {
+            @Override
+            public void onResponse(Call<Referral> call, Response<Referral> response) {
+                Referral referral = response.body();
+                referral.setStatus(status);
+                referral.setRefer_to(editReferTo.getText().toString());
+                referral.setOutcome(editOutcome.getText().toString());
+                modifyReferralInfo(referral);
+            }
+
+            @Override
+            public void onFailure(Call<Referral> call, Throwable t) {
 
             }
         });
     }
 
-    private void getAndModifyClient(int clientId, View root) {
-
-        EditText editClientName = (EditText) root.findViewById(R.id.clientDetailsEditName);
-        EditText editClientAge = (EditText) root.findViewById(R.id.clientDetailsEditAge);
-        EditText editClientLocation = (EditText) root.findViewById(R.id.clientDetailsEditLocation);
-        EditText editClientEducation = (EditText) root.findViewById(R.id.clientDetailsEditEducation);
-        EditText editClientDisability = (EditText) root.findViewById(R.id.clientDetailsEditDisability);
-        EditText editClientSocial = (EditText) root.findViewById(R.id.clientDetailsEditSocial);
-        EditText editClientHealth = (EditText) root.findViewById(R.id.clientDetailsEditHealth);
-
-        EditText editClientEducationRisk = (EditText) root.findViewById(R.id.clientDetailsEditEducationRiskLevel);
-        EditText editClientSocialRisk = (EditText) root.findViewById(R.id.clientDetailsEditSocialRiskLevel);
-        EditText editClientHealthRisk = (EditText) root.findViewById(R.id.clientDetailsEditHealthRiskLevel);
-
-        apiService.clientService.getClient(clientId).enqueue(new Callback<Client>() {
-            @Override
-            public void onResponse(Call<Client> call, Response<Client> response) {
-                Client client = response.body();
-                client.setGender(gender);
-
-                String [] clientName = editClientName.getText().toString().split(" ");
-                client.setFirstName(clientName[0]);
-                client.setLastName(clientName[1]);
-                client.setAge(Integer.parseInt(editClientAge.getText().toString()));
-                client.setLocation(editClientLocation.getText().toString());
-                client.setEducationGoal(editClientEducation.getText().toString());
-                client.setDisability(editClientDisability.getText().toString());
-                client.setSocialGoal(editClientSocial.getText().toString());
-                client.setHealthGoal(editClientHealth.getText().toString());
-                client.setEducationRisk(Integer.parseInt((editClientEducationRisk.getText().toString())));
-                client.setSocialRisk(Integer.parseInt(editClientSocialRisk.getText().toString()));
-                client.setHealthRisk(Integer.parseInt(editClientHealthRisk.getText().toString()));
-                modifyClientInfo(client);
-            }
-
-            @Override
-            public void onFailure(Call<Client> call, Throwable t) {
-
-            }
-        });
-    }
-
-    private void setupGenderSpinner(View root) {
-        String[] paths = {"Male", "Female"};
-        Spinner spinner = (Spinner) root.findViewById(R.id.clientDetailsEditGenderSpinner);
+    private void setupStatusSpinner(View root) {
+        String[] paths = {"CREATED", "RESOLVED"};
+        Spinner spinner = (Spinner) root.findViewById(R.id.referralDetailsEditStatusSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_spinner_item,paths);
 
@@ -149,51 +105,65 @@ public class ReferralDetailsEditFragment extends Fragment {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                gender = paths[position];
+                status = paths[position];
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                gender = paths[0];
+                status = paths[0];
             }
         });
     }
 
-    private void setupEditTexts(int clientId, View root) {
-        EditText editClientName = (EditText) root.findViewById(R.id.clientDetailsEditName);
-        EditText editClientAge = (EditText) root.findViewById(R.id.clientDetailsEditAge);
-        EditText editClientLocation = (EditText) root.findViewById(R.id.clientDetailsEditLocation);
-        EditText editClientEducation = (EditText) root.findViewById(R.id.clientDetailsEditEducation);
-        EditText editClientDisability = (EditText) root.findViewById(R.id.clientDetailsEditDisability);
-        EditText editClientSocial = (EditText) root.findViewById(R.id.clientDetailsEditSocial);
-        EditText editClientHealth = (EditText) root.findViewById(R.id.clientDetailsEditHealth);
+    private void setUpTextView(int textViewId, String text) {
+        TextView textView = (TextView)getView().findViewById(textViewId);
+        if(text.equals("")){
+            text="None";
+        }
+        textView.setText(text);
+    }
 
-        EditText editClientEducationRisk = (EditText) root.findViewById(R.id.clientDetailsEditEducationRiskLevel);
-        EditText editClientSocialRisk = (EditText) root.findViewById(R.id.clientDetailsEditSocialRiskLevel);
-        EditText editClientHealthRisk = (EditText) root.findViewById(R.id.clientDetailsEditHealthRiskLevel);
+    private void setUpEditTextView(int editTextViewId, String text) {
+        EditText editTextView = (EditText)getView().findViewById(editTextViewId);
+        if(text.equals("")){
+            text="None";
+        }
+        editTextView.setText(text);
+    }
 
-        apiService.clientService.getClient(clientId).enqueue(new Callback<Client>() {
+    private void setupClientTextView(int clientId) {
+        TextView clientNameTextView = (TextView)getView().findViewById(R.id.referralDetailsClientTextView);
+
+        if (apiService.isAuthenticated()) {
+            apiService.clientService.getClient(clientId).enqueue(new Callback<Client>() {
+                @Override
+                public void onResponse(Call<Client> call, Response<Client> response) {
+                    if (response.isSuccessful()) {
+                        Client client = response.body();
+                        clientNameTextView.setText(client.getFullName());
+                    }
+                }
+                @Override
+                public void onFailure(Call<Client> call, Throwable t) {
+                }
+            });
+        }
+    }
+
+
+    private void setupTexts(int referralId, View root) {
+        apiService.referralService.getReferral(referralId).enqueue(new Callback<Referral>() {
             @Override
-            public void onResponse(Call<Client> call, Response<Client> response) {
-                Client client = response.body();
-                String clientFirstName = client.getFirstName();
-                String clientLastName = client.getLastName();
-                editClientName.setText(clientFirstName + " " + clientLastName);
-                editClientAge.setText(client.getAge().toString());
-                editClientLocation.setText(client.getLocation());
-                editClientEducation.setText(client.getEducationGoal());
-                editClientDisability.setText(client.getDisability());
-                editClientSocial.setText(client.getSocialGoal());
-                editClientHealth.setText(client.getHealthGoal());
-                editClientEducationRisk.setText(client.getEducationRisk().toString());
-                editClientSocialRisk.setText(client.getSocialRisk().toString());
-                editClientHealthRisk.setText(client.getHealthRisk().toString());
-
+            public void onResponse(Call<Referral> call, Response<Referral> response) {
+                Referral referral = response.body();
+                setupClientTextView(referral.getClient());
+                setUpTextView(R.id.referralDetailsTypeTextView, referral.getServiceType());
+                setUpEditTextView(R.id.referralDetailsReferToEditTextView, referral.getRefer_to());
+                setUpEditTextView(R.id.referralDetailsOutcomeEditTextView, referral.getOutcome());
+                setUpTextView(R.id.referralDetailsServiceDetailTextView, referral.getServiceDetail().getInfo());
+                setUpTextView(R.id.referralDetailsDateCreatedTextView, referral.getFormattedDate());
             }
-
             @Override
-            public void onFailure(Call<Client> call, Throwable t) {
-
+            public void onFailure(Call<Referral> call, Throwable t) {
             }
         });
     }
@@ -206,19 +176,19 @@ public class ReferralDetailsEditFragment extends Fragment {
     }
     private void setupSubmitButton(View root) {
         Intent intent = getActivity().getIntent();
-        int clientId = intent.getIntExtra("clientId", -1);
+        int referralId = intent.getIntExtra("referralId", -1);
 
-        Button submitButton = root.findViewById(R.id.clientDetailsEditSubmitButton);
+        Button submitButton = root.findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getAndModifyClient(clientId, root);
+                getAndModifyReferral(referralId, root);
             }
         });
     }
 
     private void setupBackButton(View root) {
-        ImageView backButtonImageView = root.findViewById(R.id.clientDetailsEditBackImageView);
+        ImageView backButtonImageView = root.findViewById(R.id.referralDetailsBackImageView);
         backButtonImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

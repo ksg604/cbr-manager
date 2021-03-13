eca1bed5febdf26065fbe81376baf179e16acd26
Jonathan Tang <jct19@sfu.ca>
2021-03-13 12:21:41 -0800

    display fields to admin, make sure to save on signal

--- backend/sync/admin.py
+++ backend/sync/admin.py
@@ -6,4 +6,4 @@ from sync.models import Status
 
 @admin.register(Status)
 class Status(admin.ModelAdmin):
-    pass
+    readonly_fields = ['created_at', 'updated_at', 'client_last_updated']
--- backend/sync/models.py
+++ backend/sync/models.py
@@ -13,9 +13,12 @@ class Status(TimestampedModel):
     client_last_updated = models.DateTimeField(auto_now_add=True)
 
 
+# register all models for Status model to timestamp changes
 @receiver(post_save, sender=Client)
 def update_sync_status(sender, instance=None, created=False, **kwargs):
     if created:
         status = Status.objects.get(pk=1)
         if sender == Client:
             status.client_last_updated = timezone.now()
+
+        status.save()

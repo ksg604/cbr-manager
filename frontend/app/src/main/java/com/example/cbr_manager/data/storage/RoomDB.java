384843c0fb44e9472c0082d55f88f4d41d316042
Jonathan Tang <jct19@sfu.ca>
2021-03-14 16:18:24 -0700

    refactor roomDB, add status entity
    - add executor
    - give static db name

--- frontend/app/src/main/java/com/example/cbr_manager/data/storage/RoomDB.java
+++ frontend/app/src/main/java/com/example/cbr_manager/data/storage/RoomDB.java
@@ -9,28 +9,39 @@ import androidx.room.TypeConverters;
 
 import com.example.cbr_manager.service.client.Client;
 import com.example.cbr_manager.service.client.ClientDao;
+import com.example.cbr_manager.service.sync.Status;
+import com.example.cbr_manager.service.sync.StatusDao;
 import com.example.cbr_manager.service.visit.Visit;
 import com.example.cbr_manager.service.visit.VisitDao;
 
-@Database(entities = {Client.class, Visit.class}, version = 1, exportSchema = false)
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Client.class, Visit.class, Status.class}, version = 1, exportSchema = false)
 @TypeConverters({TimeStampConverter.class})
 public abstract class RoomDB extends RoomDatabase {
 
     public abstract ClientDao clientDao();
     public abstract VisitDao visitDao();
+    public abstract StatusDao statusDao();
 
     private static volatile RoomDB Instance;
+
+    public static String DATABASE_NAME = "app_database";
+
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
     public static RoomDB getDatabase(final Context context){
         if (Instance == null){
             synchronized (RoomDB.class){
                 if(Instance == null){
                     Instance = Room.databaseBuilder(context.getApplicationContext(),
-                            RoomDB.class, "app_database").build();
+                            RoomDB.class, DATABASE_NAME).build();
                 }
             }
         }
         return Instance;
     }
-
 }

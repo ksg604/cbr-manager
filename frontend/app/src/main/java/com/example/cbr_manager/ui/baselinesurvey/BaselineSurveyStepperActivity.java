db446dafe861495abe8af0d6ac11af7633b09c4e
Peter Tran <ttt13@sfu.ca>
2021-03-17 23:41:35 -0700

    init baseline activity

---
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/baselinesurvey/BaselineSurveyStepperActivity.java
@@ -0,0 +1,16 @@
+package com.example.cbr_manager.ui.baselinesurvey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cbr_manager.R;
+
+public class BaselineSurveyStepperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baseline_survey_stepper);
+    }
+}

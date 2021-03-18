09e52780e2589757fa8db944a1d9a92f2a769fb5
Peter Tran <ttt13@sfu.ca>
2021-03-18 11:26:24 -0700

    start on model

--- backend/baseline_survey/models.py
+++ backend/baseline_survey/models.py
@@ -5,6 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from clients.models import Client
+from baseline_survey.text_choices import AssistiveDevice, GeneralHealth, HealthServiceSatisfaction, NoSchoolReason, ChildNourishment
 
 
 # Create your models here.
@@ -13,3 +14,6 @@ class BaselineSurvey(models.model):
     user_creator = models.ForeignKey(User, on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    # health
+    general_health = models.CharField(choices=GeneralHealth.choices, max_length=50)

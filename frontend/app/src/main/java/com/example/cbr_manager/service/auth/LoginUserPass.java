811d6ffffcc028c5cfbe53beffa7a6a2534a78c9
Jonathan Tang <jct19@sfu.ca>
2021-03-18 18:56:16 -0700

    add expose tags

--- frontend/app/src/main/java/com/example/cbr_manager/service/auth/LoginUserPass.java
+++ frontend/app/src/main/java/com/example/cbr_manager/service/auth/LoginUserPass.java
@@ -2,21 +2,17 @@ package com.example.cbr_manager.service.auth;
 
 import androidx.room.ColumnInfo;
 
-import com.example.cbr_manager.BuildConfig;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import com.google.gson.annotations.Expose;
 
 public class LoginUserPass {
     @ColumnInfo(name = "auth_username")
+    @Expose
     public String username;
     @ColumnInfo(name = "auth_password")
+    @Expose
     public String password;
 
-    public LoginUserPass(String username, String password){
+    public LoginUserPass(String username, String password) {
         this.username = username;
         this.password = password;
     }

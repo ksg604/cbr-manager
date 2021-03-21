941ab6a4ca836cf1bccf0ce31f9ab7048aefb7fb
Peter Tran <ttt13@sfu.ca>
2021-03-21 15:28:18 -0700

    update baseline survey in shelter

--- frontend/app/src/main/java/com/example/cbr_manager/ui/baselinesurvey/BaselineShelterCareFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/baselinesurvey/BaselineShelterCareFragment.java
@@ -9,12 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.cbr_manager.R;
+import com.example.cbr_manager.service.baseline_survey.BaselineSurvey;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
 public class BaselineShelterCareFragment extends Fragment implements Step {
+
+    private View view;
+    private BaselineSurvey baselineSurvey;
+    private final String NOT_AVAILABLE = "N/A";
+    RadioGroup adequateRadioGroup;
+    RadioGroup essentialRadioGroup;
 
     public BaselineShelterCareFragment() {
         // Required empty public constructor
@@ -36,13 +45,22 @@ public class BaselineShelterCareFragment extends Fragment implements Step {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shelter_care, container, false);
+        view = inflater.inflate(R.layout.fragment_shelter_care, container, false);
+        adequateRadioGroup = view.findViewById(R.id.shelterAdequateRadioGroup);
+        essentialRadioGroup = view.findViewById(R.id.shelterEssentialRadioGroup);
+        return view;
     }
 
     @Nullable
     @Override
     public VerificationError verifyStep() {
+        updateBaselineSurvey();
         return null;
+    }
+
+    private void updateBaselineSurvey() {
+        baselineSurvey.setAdequateShelter(getRadioButtonString(adequateRadioGroup));
+        baselineSurvey.setAccessEssentials(getRadioButtonString(essentialRadioGroup));
     }
 
     @Override
@@ -53,5 +71,18 @@ public class BaselineShelterCareFragment extends Fragment implements Step {
     @Override
     public void onError(@NonNull VerificationError error) {
 
+    }
+
+    private String getRadioButtonString(RadioGroup radioGroup) {
+        int id = radioGroup.getCheckedRadioButtonId();
+        if (id == -1) {
+            return "";
+        }
+        RadioButton radioButton = getView().findViewById(id);
+        if (radioButton.getText().toString().trim().equals(NOT_AVAILABLE)) {
+            return "";
+        }
+
+        return radioButton.getText().toString().trim();
     }
 }

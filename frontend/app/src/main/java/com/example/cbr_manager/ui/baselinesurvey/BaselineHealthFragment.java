97d6fbfcba4e7798a9d7337f817ce0a40962c8ef
Peter Tran <ttt13@sfu.ca>
2021-03-21 13:04:41 -0700

    default radio groups in health to n/a

--- frontend/app/src/main/java/com/example/cbr_manager/ui/baselinesurvey/BaselineHealthFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/baselinesurvey/BaselineHealthFragment.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.example.cbr_manager.R;
 import com.stepstone.stepper.Step;
@@ -16,6 +18,14 @@ import com.stepstone.stepper.VerificationError;
 
 public class BaselineHealthFragment extends Fragment implements Step {
 
+    Spinner generalHealthSpinner;
+    RadioGroup rehabAccessRadioGroup;
+    RadioGroup assistiveDeviceHave;
+    RadioGroup assistiveDeviceWorking;
+    Spinner assistiveDeivceNeed;
+    Spinner healthSatisfactionSpinner;
+
+    private View view;
 
     public BaselineHealthFragment() {
         // Required empty public constructor
@@ -37,7 +47,16 @@ public class BaselineHealthFragment extends Fragment implements Step {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_baseline_health, container, false);
+        view =  inflater.inflate(R.layout.fragment_baseline_health, container, false);
+
+        generalHealthSpinner = view.findViewById(R.id.baselineHealthRateSpinner);
+        rehabAccessRadioGroup = view.findViewById(R.id.rehabAccessRadioGroup);
+        assistiveDeviceHave = view.findViewById(R.id.assistiveDeviceHaveRadioGroup);
+        assistiveDeviceWorking = view.findViewById(R.id.assistiveDeviceWorkingRadioGroup);
+        assistiveDeivceNeed = view.findViewById(R.id.baselineAssistiveDeviceNeedSpinner);
+        healthSatisfactionSpinner = view.findViewById(R.id.baselineHealthSatisfactionSpinner);
+
+        return view;
     }
 
     @Nullable

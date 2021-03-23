c529a3c1fd86a1b230b4927a4d27656d00a131ab
Peter Tran <ttt13@sfu.ca>
2021-03-23 14:21:00 -0700

    init health provision fragment

---
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/CreateVisitHealthFragment.java
@@ -0,0 +1,40 @@
+package com.example.cbr_manager.ui.createvisit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cbr_manager.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreateVisitHealthFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateVisitHealthFragment extends Fragment {
+
+    public CreateVisitHealthFragment() {
+        // Required empty public constructor
+    }
+
+    public static CreateVisitHealthFragment newInstance(String param1, String param2) {
+        CreateVisitHealthFragment fragment = new CreateVisitHealthFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_visit_health, container, false);
+    }
+}

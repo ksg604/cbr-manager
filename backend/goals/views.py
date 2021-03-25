74a595a277c519214dc133b53368420a84deaa9a
seananigan <sdales@sfu.ca>
2021-03-24 23:25:31 -0700

    add views to goals

--- backend/goals/views.py
+++ backend/goals/views.py
@@ -1,16 +1,16 @@
 # Create your views here.
 from rest_framework import viewsets, mixins
 
-from visits.models import Visit
-from visits.serializer import VisitSerializer
+from goals.models import Goal
+from goals.serializer import GoalSerializer
 
 
-class VisitViewSet(viewsets.ModelViewSet):
-    serializer_class = VisitSerializer
-    queryset = Visit.objects.all()
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+    queryset = Goal.objects.all()
 
     def get_queryset(self):
-        queryset = Visit.objects.all()
+        queryset = Goal.objects.all()
         client_id = self.request.query_params.get('client_id', None)
         if client_id is not None:
             return queryset.filter(client__id=client_id)

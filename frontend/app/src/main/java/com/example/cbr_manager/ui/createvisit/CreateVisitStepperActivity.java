38d2a371e105b3cca30255f1bc60d0af0ab9b925
Peter Tran <ttt13@sfu.ca>
2021-03-27 12:54:54 -0700

    work on uploading to server

--- frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/CreateVisitStepperActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/CreateVisitStepperActivity.java
@@ -9,21 +9,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.cbr_manager.R;
 import com.example.cbr_manager.service.APIService;
+import com.example.cbr_manager.service.client.Client;
 import com.example.cbr_manager.service.visit.Visit;
 import com.example.cbr_manager.ui.stepper.GenericStepperAdapter;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class CreateVisitStepperActivity extends AppCompatActivity implements StepperLayout.StepperListener {
 
     private StepperLayout createVisitStepperLayout;
     public int clientId = -1;
-    public int userCreatorId = -1;
+    public int userCreatorId = 1;
+    public int visitId;
     public Visit formVisitObj;
     private APIService apiService = APIService.getInstance();
+    private Client client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +61,48 @@ public class CreateVisitStepperActivity extends AppCompatActivity implements Ste
 
     @Override
     public void onCompleted(View completeButton) {
+        if (apiService.isAuthenticated()) {
+            apiService.clientService.getClient(clientId).enqueue(new Callback<Client>() {
+                @Override
+                public void onResponse(Call<Client> call, Response<Client> response) {
+                    if (response.isSuccessful()) {
+                        client = response.body();
+                        Visit visit = new Visit("", clientId, userCreatorId, client);
+                        visit.setCbrWorkerName("petertran");
+                        Call<Visit> call1 = apiService.visitService.createVisit(visit);
+                        call1.enqueue(new Callback<Visit>() {
+                            @Override
+                            public void onResponse(Call<Visit> call, Response<Visit> response) {
+                                if (response.isSuccessful()) {
+                                    visitId = response.body().getId();
+                                    Toast.makeText(CreateVisitStepperActivity.this, "Successfully created visit!", Toast.LENGTH_SHORT).show();
+                                    onSubmitSuccess();
+                                } else {
+                                    Toast.makeText(CreateVisitStepperActivity.this, "Response error creating visit.", Toast.LENGTH_LONG).show();
+                                }
+                            }
 
+                            @Override
+                            public void onFailure(Call<Visit> call, Throwable t) {
+                                Toast.makeText(CreateVisitStepperActivity.this, "Error in creating new visit.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        Toast.makeText(CreateVisitStepperActivity.this, "Response error finding client.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Client> call, Throwable t) {
+                    Toast.makeText(CreateVisitStepperActivity.this, "Error finding client.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+    private void onSubmitSuccess() {
+        finish(); // TODO: For now.
     }
 
     @Override

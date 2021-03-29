f48caef9f4926190a6c78569c20a5326341c81a5
Peter Tran <ttt13@sfu.ca>
2021-03-28 17:26:54 -0700

    education implement step

--- frontend/app/src/main/java/com/example/cbr_manager/ui/create_client/EducationRiskFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/create_client/EducationRiskFragment.java
@@ -2,15 +2,31 @@ package com.example.cbr_manager.ui.create_client;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.example.cbr_manager.R;
+import com.example.cbr_manager.service.client.Client;
+import com.stepstone.stepper.Step;
+import com.stepstone.stepper.VerificationError;
 
-public class EducationRiskFragment extends Fragment {
+public class EducationRiskFragment extends Fragment implements Step {
+
+    private View view;
+    private Client client;
+    RadioGroup riskRadioGroup;
+    EditText healthRequireEditText;
+    final Integer CRITICAL_RISK = 5;
+    final Integer HIGH_RISK = 3;
+    final Integer MEDIUM_RISK = 2;
+    final Integer LOW_RISK = 1;
 
     public EducationRiskFragment() {
         // Required empty public constructor
@@ -30,5 +46,21 @@ public class EducationRiskFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_education_risk, container, false);
+    }
+
+    @Nullable
+    @Override
+    public VerificationError verifyStep() {
+        return null;
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onError(@NonNull VerificationError error) {
+
     }
 }

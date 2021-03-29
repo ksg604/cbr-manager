aa786cde7df4185fda9b48ff2307933f0835023b
Peter Tran <ttt13@sfu.ca>
2021-03-28 17:38:31 -0700

    gather data social risk

--- frontend/app/src/main/java/com/example/cbr_manager/ui/create_client/SocialRiskFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/create_client/SocialRiskFragment.java
@@ -2,15 +2,32 @@ package com.example.cbr_manager.ui.create_client;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.cbr_manager.R;
+import com.example.cbr_manager.service.client.Client;
+import com.stepstone.stepper.Step;
+import com.stepstone.stepper.VerificationError;
 
-public class SocialRiskFragment extends Fragment {
+public class SocialRiskFragment extends Fragment implements Step {
+
+    private View view;
+    private Client client;
+    RadioGroup riskRadioGroup;
+    EditText socialRequireEditText;
+    final Integer CRITICAL_RISK = 5;
+    final Integer HIGH_RISK = 3;
+    final Integer MEDIUM_RISK = 2;
+    final Integer LOW_RISK = 1;
 
     public SocialRiskFragment() {
         // Required empty public constructor
@@ -29,6 +46,44 @@ public class SocialRiskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_social_risk, container, false);
+        view = inflater.inflate(R.layout.fragment_social_risk, container, false);
+        client = ((CreateClientStepperActivity) getActivity()).formClientObj;
+        riskRadioGroup = view.findViewById(R.id.socialRiskRatingRadioGroup);
+        socialRequireEditText = view.findViewById(R.id.socialNeedsEditTextTextMultiLine);
+        return view;
+    }
+
+    @Nullable
+    @Override
+    public VerificationError verifyStep() {
+        client.setSocialRisk(convertRiskRating(riskRadioGroup));
+        client.setSocialRequire(socialRequireEditText.getText().toString());
+        return null;
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onError(@NonNull VerificationError error) {
+
+    }
+
+    private Integer convertRiskRating(RadioGroup riskRadioGroup) {
+        int radioButtonId = riskRadioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton = getView().findViewById(radioButtonId);
+        String risk = radioButton.getText().toString().trim().toLowerCase();
+        switch (risk) {
+            case "critical":
+                return CRITICAL_RISK;
+            case "high":
+                return HIGH_RISK;
+            case "medium":
+                return MEDIUM_RISK;
+            default:
+                return LOW_RISK;
+        }
     }
 }

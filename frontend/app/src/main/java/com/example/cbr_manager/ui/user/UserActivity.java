8e10f643e3f0a22881a9445c4137fcb3a275463a
Jonathan Tang <jct19@sfu.ca>
2021-03-29 17:10:41 -0700

    enable logouts

--- frontend/app/src/main/java/com/example/cbr_manager/ui/user/UserActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/user/UserActivity.java
@@ -1,16 +1,26 @@
 package com.example.cbr_manager.ui.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cbr_manager.R;
+import com.example.cbr_manager.ui.AuthViewModel;
+import com.example.cbr_manager.ui.login.LoginActivity;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class UserActivity extends AppCompatActivity {
 
     public final static String TAG = "UserActivity";
     public final static String KEY_USER_ID = "USER_ID";
+    private AuthViewModel authViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +29,28 @@ public class UserActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("User Profile");
 
+        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
 
         int userID = getIntent().getIntExtra(KEY_USER_ID, -1);
-        Log.d(TAG, "onCreate: " + userID);
+
+        setUpLoginButton();
+    }
+
+    private void setUpLoginButton(){
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLogout();
+            }
+        });
+    }
+
+    private void onLogout(){
+        authViewModel.logout();
+        Intent intent = new Intent(this, LoginActivity.class);
+        // clears activity stack to start anew
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }

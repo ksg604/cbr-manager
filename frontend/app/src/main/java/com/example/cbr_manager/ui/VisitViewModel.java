eab1dfb1903b038e9a995b2cf0f375d29692b0c7
Jonathan Tang <jct19@sfu.ca>
2021-03-31 16:01:24 -0700

    create visit model, and repository

--- frontend/app/src/main/java/com/example/cbr_manager/ui/VisitViewModel.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/VisitViewModel.java
@@ -1,16 +1,19 @@
 package com.example.cbr_manager.ui;
 
 import androidx.lifecycle.SavedStateHandle;
+import androidx.lifecycle.ViewModel;
 
 import com.example.cbr_manager.repository.VisitRepository;
 import com.example.cbr_manager.service.visit.Visit;
 
 import javax.inject.Inject;
 
+import dagger.hilt.android.lifecycle.HiltViewModel;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 
-public class VisitViewModel {
+@HiltViewModel
+public class VisitViewModel extends ViewModel {
     private final SavedStateHandle savedStateHandle;
     private VisitRepository visitRepository;
 

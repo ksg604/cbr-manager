6f60413376839ac3c6dc4e60c02f19136c487429
Jonathan Tang <jct19@sfu.ca>
2021-03-31 16:55:20 -0700

    have VisitsFragment use VisitViewModel

--- frontend/app/src/main/java/com/example/cbr_manager/ui/visits/VisitsFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/visits/VisitsFragment.java
@@ -10,16 +10,19 @@ import android.widget.SearchView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cbr_manager.R;
 import com.example.cbr_manager.service.APIService;
-import com.example.cbr_manager.service.client.Client;
 import com.example.cbr_manager.service.visit.Visit;
+import com.example.cbr_manager.ui.VisitViewModel;
 import com.example.cbr_manager.ui.clientdetails.ClientDetailsActivity;
 import com.example.cbr_manager.ui.clientdetails.ClientDetailsFragment;
 import com.example.cbr_manager.ui.visitdetails.VisitDetailsActivity;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.Timestamp;
 import java.text.Format;
@@ -27,24 +30,27 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import dagger.hilt.android.AndroidEntryPoint;
+import io.reactivex.observers.DisposableObserver;
 
-public class VisitsFragment extends Fragment implements VisitsRecyclerItemAdapter.OnItemListener{
-
+@AndroidEntryPoint
+public class VisitsFragment extends Fragment implements VisitsRecyclerItemAdapter.OnItemListener {
+    private static final String TAG = "VisitsFragment";
+    private static int NO_SPECIFIC_CLIENT = -1;
+    ArrayList<VisitsRecyclerItem> visitsRecyclerItems = new ArrayList<>();
     private RecyclerView visitsRecyclerView;
     private VisitsRecyclerItemAdapter adapter;
     private RecyclerView.LayoutManager visitsLayoutManager;
-    private static int NO_SPECIFIC_CLIENT = -1;
     private int clientId = NO_SPECIFIC_CLIENT;
-    ArrayList<VisitsRecyclerItem> visitsRecyclerItems = new ArrayList<>();
 
-    private APIService apiService = APIService.getInstance();
+    private VisitViewModel visitViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        visitViewModel = new ViewModelProvider(this).get(VisitViewModel.class);
+
         int clientId = NO_SPECIFIC_CLIENT;
 
         FragmentActivity activity = getActivity();
@@ -55,7 +61,7 @@ public class VisitsFragment extends Fragment implements VisitsRecyclerItemAdapte
         if (activity instanceof ClientDetailsActivity) {
             clientDetailsActivity = (ClientDetailsActivity) activity;
             if (clientDetailsActivity != null) {
-                fragment = (ClientDetailsFragment)clientDetailsActivity.getSupportFragmentManager().findFragmentById(R.id.fragment_client_details);
+                fragment = (ClientDetailsFragment) clientDetailsActivity.getSupportFragmentManager().findFragmentById(R.id.fragment_client_details);
                 clientId = fragment.getClientId();
             }
         }
@@ -88,61 +94,54 @@ public class VisitsFragment extends Fragment implements VisitsRecyclerItemAdapte
 
         return root;
     }
+
     public void fetchVisitsToList(List<VisitsRecyclerItem> visitUIList) {
-        if (apiService.isAuthenticated()) {
-            apiService.visitService.getVisits().enqueue(new Callback<List<Visit>>() {
-                @Override
-                public void onResponse(Call<List<Visit>> visitCall, Response<List<Visit>> response) {
-                    if (response.isSuccessful()) {
-                        List<Visit> visitList = response.body();
-                        for (Visit visit : visitList) {
-                            int currClientID = visit.getClientId();
-
-                            if (clientId == NO_SPECIFIC_CLIENT || visit.getClientId() == clientId) {
-                                Call<Client> clientIdCall = apiService.clientService.getClient(currClientID);
-                                clientIdCall.enqueue(new Callback<Client>() {
-                                    @Override
-                                    public void onResponse(Call<Client> clientCall, Response<Client> response) {
-                                        if (response.isSuccessful()) {
-                                            Client client = response.body();
-                                            visit.setClient(client);
-                                            Timestamp datetimeCreated = visit.getDatetimeCreated();
-                                            Format formatter = new SimpleDateFormat("dd-MM-yyyy");
-                                            String formattedDate = formatter.format(datetimeCreated);
-
-                                            String purpose = "";
-                                            if (visit.isCBRPurpose()) {
-                                                purpose += "CBR ";
-                                            }
-                                            if (visit.isDisabilityReferralPurpose()) {
-                                                purpose += "Disability Referral ";
-                                            }
-                                            if (visit.isDisabilityFollowUpPurpose()) {
-                                                purpose += "Disability Follow up";
-                                            }
-                                            if (purpose.equals("")) {
-                                                purpose = "No purpose indicated.";
-                                            }
-
-                                            visitUIList.add(new VisitsRecyclerItem(formattedDate, visit.getClient().getFullName(), visit, purpose, visit.getLocationDropDown()));
-                                        }
-                                        adapter.notifyDataSetChanged();
-                                    }
-                                    @Override
-                                    public void onFailure(Call<Client> call, Throwable t) {
-                                    }
-                                });
-                            }
-                        }
-                    }
+        visitViewModel.getVisits().subscribe(new DisposableObserver<Visit>() {
+            @Override
+            public void onNext(@io.reactivex.annotations.NonNull Visit visit) {
+                if (clientId == NO_SPECIFIC_CLIENT || visit.getClientId() == clientId) {
+                    VisitsRecyclerItem visitsRecyclerItem = createVisitRecycleItem(visit);
+                    visitUIList.add(visitsRecyclerItem);
                 }
+            }
 
-                @Override
-                public void onFailure(Call<List<Visit>> call, Throwable t) {
+            @Override
+            public void onError(@io.reactivex.annotations.NonNull Throwable e) {
 
-                }
-            });
+            }
+
+            @Override
+            public void onComplete() {
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private VisitsRecyclerItem createVisitRecycleItem(Visit visit) {
+        Timestamp datetimeCreated = visit.getDatetimeCreated();
+        Format formatter = new SimpleDateFormat("dd-MM-yyyy");
+        String formattedDate = formatter.format(datetimeCreated);
+        String purpose = formatPurposeString(visit);
+        return new VisitsRecyclerItem(formattedDate,
+                visit.getClient().getFullName(), visit, purpose, visit.getLocationDropDown());
+    }
+
+    @NotNull
+    private String formatPurposeString(@io.reactivex.annotations.NonNull Visit visit) {
+        String purpose = "";
+        if (visit.isCBRPurpose()) {
+            purpose += "CBR ";
         }
+        if (visit.isDisabilityReferralPurpose()) {
+            purpose += "Disability Referral ";
+        }
+        if (visit.isDisabilityFollowUpPurpose()) {
+            purpose += "Disability Follow up";
+        }
+        if (purpose.equals("")) {
+            purpose = "No purpose indicated.";
+        }
+        return purpose;
     }
 
 

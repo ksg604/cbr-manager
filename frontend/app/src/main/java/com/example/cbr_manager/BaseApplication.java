f8d61aea7f08c84cd1aea286ef272c623ab1e472
Jonathan Tang <jct19@sfu.ca>
2021-04-01 12:12:02 -0700

    initialize AndroidThreeTen to get access to local timezones
    specified here https://github.com/JakeWharton/ThreeTenABP

--- frontend/app/src/main/java/com/example/cbr_manager/BaseApplication.java
+++ frontend/app/src/main/java/com/example/cbr_manager/BaseApplication.java
@@ -2,8 +2,15 @@ package com.example.cbr_manager;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
 public final class BaseApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidThreeTen.init(this);
+    }
 }

79e228048fea3a1228354a4c32a78743f6f55ef7
ftran <ftran@anywhere.com>
2021-04-05 16:32:17 -0700

    Resolving conflict

--- frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/CreateVisitStepperActivity.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/createvisit/CreateVisitStepperActivity.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cbr_manager.R;

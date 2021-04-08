385cd86180995516d5e0896f516c065c23038e7a
Jonathan Tang <jct19@sfu.ca>
2021-04-08 10:35:10 -0700

    optimize imports, remove unused apiService

--- frontend/app/src/main/java/com/example/cbr_manager/ui/clientlist/ClientListFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/clientlist/ClientListFragment.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,45 +18,37 @@ import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cbr_manager.R;
-import com.example.cbr_manager.service.APIService;
 import com.example.cbr_manager.service.client.Client;
 import com.example.cbr_manager.ui.ClientViewModel;
 import com.example.cbr_manager.ui.clientdetails.ClientDetailsActivity;
 import com.example.cbr_manager.ui.create_client.CreateClientStepperActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class ClientListFragment extends Fragment implements ClientListItemAdapter.OnItemClickListener {
 
+    private static final String[] locationPaths = {"Any", "BidiBidi Zone 1", "BidiBidi Zone 2", "BidiBidi Zone 3", "BidiBidi Zone 4", "BidiBidi Zone 5",
+            "Palorinya Basecamp", "Palorinya Zone 1", "Palorinya Zone 2", "Palorinya Zone 3"};
+    private static final String[] disabilityPaths = {"Any", "Amputee", "Polio", "Spinal Cord Injury", "Cerebral Palsy", "Spina Bifada",
+            "Hydrocephalus", "Visual Impairment", "Hearing Impairment", "Don't Know", "Other"};
+    private static final String[] genderPaths = {"Any", "Male", "Female"};
     private RecyclerView clientListRecyclerView;
     private ClientListItemAdapter clientListAdapter;
     private RecyclerView.LayoutManager clientListLayoutManager;
-    private APIService apiService = APIService.getInstance();
     private ClientViewModel clientViewModel;
-
     private Spinner genderSpinner;
     private Spinner locationSpinner;
     private Spinner disabilitySpinner;
-    private String genderTag="";
-    private String locationTag="";
-    private String disabilityTag="";
+    private String genderTag = "";
+    private String locationTag = "";
+    private String disabilityTag = "";
     private SearchView clientSearch;
-
-    private static final String[] locationPaths = {"Any","BidiBidi Zone 1", "BidiBidi Zone 2", "BidiBidi Zone 3", "BidiBidi Zone 4", "BidiBidi Zone 5",
-            "Palorinya Basecamp", "Palorinya Zone 1", "Palorinya Zone 2", "Palorinya Zone 3"};
-    private static final String[] disabilityPaths = {"Any","Amputee", "Polio", "Spinal Cord Injury", "Cerebral Palsy", "Spina Bifada",
-            "Hydrocephalus", "Visual Impairment", "Hearing Impairment", "Don't Know", "Other"};
-    private static final String[] genderPaths = {"Any","Male", "Female"};
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -88,7 +79,6 @@ public class ClientListFragment extends Fragment implements ClientListItemAdapte
         clientSearch = root.findViewById(R.id.clientSearchView);
 
 
-
         clientSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -97,7 +87,7 @@ public class ClientListFragment extends Fragment implements ClientListItemAdapte
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                clientListAdapter.getFilterWithTags(genderTag,disabilityTag,locationTag).filter(newText);
+                clientListAdapter.getFilterWithTags(genderTag, disabilityTag, locationTag).filter(newText);
                 return true;
             }
         });
@@ -114,21 +104,22 @@ public class ClientListFragment extends Fragment implements ClientListItemAdapte
         return spinner;
     }
 
-    private void setUpSpinnerListener(Spinner spinner){
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+    private void setUpSpinnerListener(Spinner spinner) {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> spinner, View view, int pos, long unused) {
                 String tag = spinner.getItemAtPosition(pos).toString().toLowerCase().trim();
-                if(spinner == genderSpinner){
+                if (spinner == genderSpinner) {
                     genderTag = tag;
-                } else if(spinner == disabilitySpinner){
+                } else if (spinner == disabilitySpinner) {
                     disabilityTag = tag;
-                } else if(spinner == locationSpinner){
+                } else if (spinner == locationSpinner) {
                     locationTag = tag;
                 }
                 CharSequence newText = clientSearch.getQuery();
-                clientListAdapter.getFilterWithTags(genderTag,disabilityTag,locationTag).filter(newText);
+                clientListAdapter.getFilterWithTags(genderTag, disabilityTag, locationTag).filter(newText);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> spinner) {
             }

ca8ebc04689c50baf4049d2c779f3a147145f5e5
Peter Tran <ttt13@sfu.ca>
2021-04-07 18:30:30 -0700

    init goal history

---
+++ frontend/app/src/main/java/com/example/cbr_manager/goalhistory/GoalHistoryFragment.java
@@ -0,0 +1,35 @@
+package com.example.cbr_manager.goalhistory;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cbr_manager.R;
+
+public class GoalHistoryFragment extends Fragment {
+
+    public GoalHistoryFragment() {
+        // Required empty public constructor
+    }
+
+    public static GoalHistoryFragment newInstance(String param1, String param2) {
+        GoalHistoryFragment fragment = new GoalHistoryFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_goal_history, container, false);
+    }
+}

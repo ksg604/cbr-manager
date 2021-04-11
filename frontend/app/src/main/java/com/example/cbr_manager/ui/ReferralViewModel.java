08e9385a2214b27de487ccea919c3b0394bf5ad4
Andrew Huang <ach16@sfu.ca>
2021-04-10 20:40:45 -0700

    add the methods to match that of visit and clients so offline database can work

--- frontend/app/src/main/java/com/example/cbr_manager/ui/ReferralViewModel.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/ReferralViewModel.java
@@ -1,15 +1,20 @@
 package com.example.cbr_manager.ui;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
 import com.example.cbr_manager.repository.ReferralRepository;
 import com.example.cbr_manager.service.referral.Referral;
+import com.example.cbr_manager.service.referral.Referral;
+
+import java.io.File;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
-import io.reactivex.Observable;
+import io.reactivex.Completable;
 import io.reactivex.Single;
 
 @HiltViewModel
@@ -23,15 +28,23 @@ public class ReferralViewModel extends ViewModel {
         this.referralRepository = referralRepository;
     }
 
-    public Single<Referral> getReferral(int id) {
+    public LiveData<Referral> getReferral(int id){
         return referralRepository.getReferral(id);
     }
 
-    public Observable<Referral> getReferrals(){
-        return referralRepository.getReferrals();
+    public LiveData<List<Referral>> getReferralsAsLiveData() {
+        return referralRepository.getReferralsAsLiveData();
     }
 
     public Single<Referral> createReferral(Referral referral) {
         return referralRepository.createReferral(referral);
+    }
+
+    public Completable modifyReferral(Referral referral) {
+        return referralRepository.modifyReferral(referral);
+    }
+
+    public Completable uploadPhoto(File file, Referral referral) {
+        return this.referralRepository.uploadPhoto(file, referral);
     }
 }

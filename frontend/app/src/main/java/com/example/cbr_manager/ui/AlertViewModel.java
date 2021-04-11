427d5e3b3a0ffb8b2697759ae110b46cd0623684
Andrew Huang <ach16@sfu.ca>
2021-04-11 07:58:32 -0700

    create ViewModel as interface for double database operations in UI

---
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/AlertViewModel.java
@@ -0,0 +1,48 @@
+package com.example.cbr_manager.ui;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.SavedStateHandle;
+import androidx.lifecycle.ViewModel;
+
+import com.example.cbr_manager.repository.AlertRepository;
+import com.example.cbr_manager.service.alert.Alert;
+
+import java.io.File;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.Completable;
+import io.reactivex.Single;
+
+@HiltViewModel
+public class AlertViewModel extends ViewModel {
+    private final AlertRepository alertRepository;
+
+    private final SavedStateHandle savedStateHandle;
+
+    @Inject
+    public AlertViewModel(SavedStateHandle savedStateHandle, AlertRepository alertRepository) {
+        this.savedStateHandle = savedStateHandle;
+        this.alertRepository = alertRepository;
+    }
+
+    public LiveData<List<Alert>> getAllAlerts() {
+        return this.alertRepository.getAlertsAsLiveData();
+    }
+
+    public LiveData<Alert> getAlert(int id) {
+        return alertRepository.getAlert(id);
+    }
+
+    public Single<Alert> getAlertAsSingle(int id) {
+        return alertRepository.getAlertAsSingle(id);
+    }
+
+    public Single<Alert> createAlert(Alert alert) {
+        return this.alertRepository.createAlert(alert);
+    }
+
+
+}

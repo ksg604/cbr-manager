2f3f88fdeeb013233dc4b6328c09e635dd0f614e
Peter Tran <ttt13@sfu.ca>
2021-04-11 20:55:59 -0700

    remove dead code

--- frontend/app/src/main/java/com/example/cbr_manager/ui/homepage/HomepageFragment.java
+++ frontend/app/src/main/java/com/example/cbr_manager/ui/homepage/HomepageFragment.java
@@ -23,8 +23,6 @@ import com.example.cbr_manager.ui.map.MapActivity;
 
 public class HomepageFragment extends Fragment {
     private APIService apiService = APIService.getInstance();
-    private ImageButton newClientButton, newVisitButton, dashboardButton;
-    private ImageButton newReferralButton, clientListButton, syncButton;
     private CardView newClientCard, dashboardCard, clientListCard, surveyCard, newVisitCard, newReferralCard, syncCard, mapCard;
     private ImageButton newSurvey;
     private final int NEW_VISIT_CODE = 100;
@@ -46,14 +44,6 @@ public class HomepageFragment extends Fragment {
                 startActivity(intent);
             }
         });
-//        newClientButton = view.findViewById(R.id.newClientButton);
-//        newClientButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(view.getContext(), CreateClientStepperActivity.class);
-//                startActivity(intent);
-//            }
-//        });
 
         newVisitCard = view.findViewById(R.id.newVisitCardView);
         newVisitCard.setOnClickListener(new View.OnClickListener() {
@@ -64,16 +54,6 @@ public class HomepageFragment extends Fragment {
                 startActivity(intent);
             }
         });
-//        newVisitButton = view.findViewById(R.id.newVisitButton);
-//        newVisitButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Intent intent = new Intent(HomepageActivity.this, CreateVisitActivity.class);
-//                Intent intent = new Intent(view.getContext(), ClientSelectorActivity.class);
-//                intent.putExtra("CODE", NEW_VISIT_CODE);
-//                startActivity(intent);
-//            }
-//        });
 
         dashboardCard = view.findViewById(R.id.dashboardCardView);
         dashboardCard.setOnClickListener(new View.OnClickListener() {
@@ -83,15 +63,6 @@ public class HomepageFragment extends Fragment {
                         .navigate(R.id.action_nav_home_to_nav_dashboard);
             }
         });
-//
-//        dashboardButton = view.findViewById(R.id.dashboardButton);
-//        dashboardButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NavHostFragment.findNavController(HomepageFragment.this)
-//                        .navigate(R.id.action_nav_home_to_nav_dashboard);
-//            }
-//        });
 
         newReferralCard = view.findViewById(R.id.newReferralCardView);
         newReferralCard.setOnClickListener(new View.OnClickListener() {
@@ -102,15 +73,6 @@ public class HomepageFragment extends Fragment {
                 startActivity(intent);
             }
         });
-//        newReferralButton = view.findViewById(R.id.newReferralButton);
-//        newReferralButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(view.getContext(), ClientSelectorActivity.class);
-//                intent.putExtra("CODE", NEW_REFERRAL_CODE);
-//                startActivity(intent);
-//            }
-//        });
 
         clientListCard = view.findViewById(R.id.clientListCardView);
         clientListCard.setOnClickListener(new View.OnClickListener() {
@@ -120,14 +82,6 @@ public class HomepageFragment extends Fragment {
                         .navigate(R.id.action_nav_home_to_nav_client_list);
             }
         });
-//        clientListButton = view.findViewById(R.id.clientListButton);
-//        clientListButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                NavHostFragment.findNavController(HomepageFragment.this)
-//                        .navigate(R.id.action_nav_home_to_nav_client_list);
-//            }
-//        });
 
         syncCard = view.findViewById(R.id.syncCardView);
         syncCard.setOnClickListener(new View.OnClickListener() {
@@ -136,14 +90,6 @@ public class HomepageFragment extends Fragment {
 
             }
         });
-
-//        syncButton = view.findViewById(R.id.syncButton);
-//        syncButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
 
         surveyCard = view.findViewById(R.id.baselineSurveyCardView);
         surveyCard.setOnClickListener(new View.OnClickListener() {
@@ -154,15 +100,6 @@ public class HomepageFragment extends Fragment {
                 startActivity(intent);
             }
         });
-//        newSurvey = view.findViewById(R.id.baselineSurveyImageButton);
-//        newSurvey.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getContext(), ClientSelectorActivity.class);
-//                intent.putExtra("CODE", NEW_BASELINE_CODE);
-//                startActivity(intent);
-//            }
-//        });
 
         mapCard = view.findViewById(R.id.mapCardView);
         mapCard.setOnClickListener(new View.OnClickListener() {
@@ -172,14 +109,6 @@ public class HomepageFragment extends Fragment {
                 startActivity(intent);
             }
         });
-//       ImageView mapButton = view.findViewById(R.id.mapButton);
-//        mapButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getContext(), MapActivity.class);
-//                startActivity(intent);
-//            }
-//        });
 
         return view;
     }
